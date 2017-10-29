9c59e8532f00d22be97d5db94bf38741aa75cafd
Maksim <max.kvant@gmail.com>
2017-10-29 18:17:44 +0300

    added hw01

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,45 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+data class Dancer(val x0: Int, val y0: Int, val t: Int, val pos: Int) {
+    init {
+        require((x0 == 0) != (y0 == 0))
+    }
 
-    return words.joinToString(separator = " ")
+    val potential: Int = x0 + y0 - t
+    val vertical: Boolean = x0 == 0
+}
+
+data class Point(val x: Int, val y: Int, val pos: Int)
+
+fun solve(w: Int, h: Int, dancers: List<Dancer>): List<Point> {
+    val groupsByPotential: Map<Int, List<Dancer>> = dancers.groupBy { dancer -> dancer.potential }
+
+    return groupsByPotential.values.flatMap { group ->
+        val curDancers: List<Dancer> = group
+                .sortedWith(compareBy<Dancer> { dancer -> dancer.x0 }.thenByDescending { dancer -> dancer.y0 })
+
+        val resPoints = curDancers.map { dancer ->
+            when (dancer.vertical) {
+                true -> Point(w, dancer.y0, dancer.pos)
+                false -> Point(dancer.x0, h, dancer.pos)
+            }
+        }.sortedWith(compareBy<Point> { p -> p.x }.thenByDescending { p -> p.y })
+
+        curDancers.zip(resPoints, { dancer, point -> Point(point.x, point.y, dancer.pos) })
+    }.sortedBy { point -> point.pos }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val (n: Int, w: Int, h: Int) = readLine()!!.split(" ").map(String::toInt)
+    val dancers: List<Dancer> = (0 until n).map { i ->
+        val (g: Int, p: Int, t: Int) = readLine()!!.split(" ").map(String::toInt)
+        val (x, y) = when (g) {
+            1 -> Pair(p, 0)
+            2 -> Pair(0, p)
+            else -> throw RuntimeException("g=${g} isn't in {1, 2}")
+        }
+        Dancer(x, y, t, i)
+    }.toList()
+    val res: List<Point> = solve(w, h, dancers)
+    res.forEach { point -> println("${point.x} ${point.y}") }
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,51 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
 import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun test1() {
+        val w = 10
+        val h = 8
+        val dancers = listOf(
+                Dancer(1, 0, 10, 0),
+                Dancer(4, 0, 13, 1),
+                Dancer(7, 0, 1, 2),
+                Dancer(8, 0, 2, 3),
+                Dancer(0, 2, 0, 4),
+                Dancer(0, 5, 14, 5),
+                Dancer(0, 6, 0, 6),
+                Dancer(0, 6, 1, 7)
+        )
+
+        assertThat(solve(w, h, dancers), `is`(listOf(
+                Point(4, 8, 0),
+                Point(10, 5, 1),
+                Point(8, 8, 2),
+                Point(10, 6, 3),
+                Point(10, 2, 4),
+                Point(1, 8, 5),
+                Point(7, 8, 6),
+                Point(10, 6, 7)
+        )))
+    }
+
+    @Test
+    fun test2() {
+        val w = 2
+        val h = 3
+        val dancers = listOf(
+                Dancer(1, 0, 2, 0),
+                Dancer(0, 1, 1, 1),
+                Dancer(1, 0, 5, 2)
+        )
+
+        assertThat(solve(w, h, dancers), `is`(listOf(
+                Point(1, 3, 0),
+                Point(2, 1, 1),
+                Point(1, 3, 2)
+        )))
     }
 }
